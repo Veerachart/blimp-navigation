1b819d200db8d9aa07ba1a9df728a72ebdb299ce
Veerachart Srisamosorn <veerachart.sr@gmail.com>
2018-03-23 10:57:17 +0900

    Removed unneccessary headers.

--- src/BlimpHumanTracker.cpp
+++ src/BlimpHumanTracker.cpp
@@ -1,9 +1,9 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <cvaux.h>
+//#include <cvaux.h>
 #include <math.h>
-#include <cxcore.h>
+//#include <cxcore.h>
 #include "opencv2/opencv.hpp"
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
